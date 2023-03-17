7b04280db97f0a0d9b88229d44abd734972fea7d
Ben Cos <52817096+BenCos17@users.noreply.github.com>
2023-03-17 22:12:05 +0000

    add perm checks

--- Post/post.py
+++ Post/post.py
@@ -6,7 +6,18 @@ class Post(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    async def can_send_message(self, ctx):
+        if ctx.channel.permissions_for(ctx.me).send_messages and ctx.channel.permissions_for(ctx.author).send_messages:
+            return True
+        else:
+            if not ctx.channel.permissions_for(ctx.me).send_messages:
+                await ctx.send("I do not have permission to send messages in this channel.")
+            if not ctx.channel.permissions_for(ctx.author).send_messages:
+                await ctx.send("You do not have permission to send messages in this channel.")
+            return False
+
     @commands.command()
+    @commands.check(lambda ctx: self.can_send_message(ctx))
     async def post(self, ctx, title, description, color, channel: discord.TextChannel):
         try:
             if color.startswith("#"):
@@ -23,15 +34,16 @@ class Post(commands.Cog):
             await ctx.send(f"Invalid color '{color}'.")
 
         except discord.Forbidden:
-            await ctx.send("I do not have permission to send messages in that channel. Please make sure I have the necessary permissions to send messages and try again.")
+            await ctx.send(f"I do not have permission to send messages in {channel.mention}. Please make sure I have the necessary permissions to send messages and try again. If you are not sure what permissions I need, please check your own permissions or ask a server administrator.")
 
     @commands.command()
+    @commands.check(lambda ctx: self.can_send_message(ctx))
     async def postmessage(self, ctx, message, channel: discord.TextChannel):
         try:
             await channel.send(message)
 
         except discord.Forbidden:
-            await ctx.send("I do not have permission to send messages in that channel. Please make sure I have the necessary permissions to send messages and try again.")
+            await ctx.send(f"I do not have permission to send messages in {channel.mention}. Please make sure I have the necessary permissions to send messages and try again. If you are not sure what permissions I need, please check your own permissions or ask a server administrator.")
 
 def setup(bot):
     bot.add_cog(Post(bot))
